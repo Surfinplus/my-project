b331907d9007d658bcd2601f052ff66a5258fc59
Eren Çoban <162314610+Surfinplus@users.noreply.github.com>
2024-05-18 15:40:14 +0300

    commit9

---
+++ contact.php
@@ -0,0 +1,14 @@
+<?php
+  $email = $_POST['email'];
+  $name = $_POST['name'];
+  $message = $_POST['message'];
+
+
+
+  print ( "<div> <h2>" . $email . "</h2></div>");
+  print ( "<div> <h2>" . $name . "</h2></div>");
+  print ( "<div> <h2>" . $message . "</h2></div>");
+
+  
+  ?>
+
